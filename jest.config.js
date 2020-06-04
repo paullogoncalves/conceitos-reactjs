578005486266a4e1fd5c5cdce6878d7a0577b920
Paulo Goncalves <paullogoncalves@gmail.com>
2020-06-04 15:55:02 -0300

    desafio/concluido-conceitos-reactjs

--- jest.config.js
+++ jest.config.js
@@ -1,1 +1,6 @@
-module.exports = {};
+module.exports = {
+  verbose: true,
+  collectCoverageFrom: [ 'src/**/*test.js' ],
+  coverageReporters: ['text-summary', 'html'],
+  testPathIgnorePatterns: ['/node_modules/'],
+};
--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [ repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then(response => {
+      setRepositories(response.data)
+    })
+  }, [])
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('/repositories', {
+      title: "AplicativoNOVO reactJS22",
+      url: "http://github.com/PauloGonçalves",
+      techs: ["nodejs, exprex.js"],
+      likes: 0
+    });
+
+    const repositorie = response.data;
+
+    setRepositories([...repositories, repositorie])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
-  }
+    const repositoriesModified = repositories.filter(repository => repository.id !== id )
+    
+    setRepositories(repositoriesModified)
+  } 
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        {repositories.map(repo =>  
+        <li key={repo.id}>
+          {repo.title}
+          <button onClick={() => handleRemoveRepository(repo.id)} >
             Remover
           </button>
         </li>
+        )}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
